39e453d3b9a154cf66bc4527c8ac4ea20e5c3cc6
Antoine Kingue <contact@antoinek.fr>
2024-09-10 12:05:37 +0200

    🚨 fix eslint

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { CalendarDays, MapPin, Music, Ticket } from "lucide-react";
+import { MapPin, Music, Ticket } from "lucide-react";
 
 import { Button } from "@acme/ui/button";
 import {
