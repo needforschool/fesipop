6a3639dbe35e31d5b88923989ceab35aa06c98c2
CesTheo <theo.maudet@icloud.com>
2024-09-10 16:04:44 +0200

    add(): logo & navvar

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -19,11 +19,12 @@ export default function LandingPage() {
     <div className="flex min-h-screen flex-col">
       <header className="flex h-14 items-center px-4 lg:px-6">
         <Link className="flex items-center justify-center" href="#">
-          <Music className="mr-2 h-6 w-6" />
-          <span className="font-bold">FestivaApp</span>
+          {/* <Music className="mr-2 h-6 w-6" /> */}
+          <Image alt="logo" height="50" src="/logo.png" width="50" />
+          <span className="font-bold">FESIPOP</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
-          <Link
+          {/* <Link
             className="text-sm font-medium underline-offset-4 hover:underline"
             href="#"
           >
@@ -46,7 +47,7 @@ export default function LandingPage() {
             href="#"
           >
             Contact
-          </Link>
+          </Link> */}
         </nav>
       </header>
       <main className="flex-1">
