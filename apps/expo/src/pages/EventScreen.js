ff378ddfc0c35354a0a67aa41dda05c9b9214c6c
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2024-09-10 12:08:20 +0200

    feat: add event screen with event card

---
+++ apps/expo/src/pages/EventScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, ImageBackground, StyleSheet, ScrollView } from 'react-native';
+import EventCard from "../component/EventCard";
+const backgroundImage = require('../../assets/icon.png');
+const groupImage = require('../../assets/eventimg.jpg');
+
+const EventScreen = () => {
+    return (
+        <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+            <View style={styles.overlay}>
+                <ScrollView
+                    contentContainerStyle={styles.scrollViewContainer}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <Text style={styles.headerText}>EVENEMENTS</Text>
+                    {/* Génère plusieurs cartes d'événements */}
+                    {Array(7).fill(0).map((_, index) => (
+                        <EventCard
+                            key={index}
+                            city="PARIS"
+                            groupName="TWICE"
+                            imageSource={groupImage}
+                        />
+                    ))}
+                </ScrollView>
+            </View>
+        </ImageBackground>
+    );
+};
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    overlay: {
+        flex: 1,
+    },
+    scrollViewContainer: {
+        paddingVertical: 25,
+        paddingHorizontal: 20,
+        paddingBottom: 120,
+    },
+    headerText: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 35,
+        marginBottom: 30,
+    },
+});
+
+export default EventScreen;
